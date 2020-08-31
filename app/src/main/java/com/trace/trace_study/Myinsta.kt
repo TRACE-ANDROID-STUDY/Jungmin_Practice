14526a8337840ddc228d10c380608c6f72bf4a9b
Jungmin <ljm9748@naver.com>
2020-09-01 01:08:19 +0900

    Add 1st, 2nd

---
+++ app/src/main/java/com/trace/trace_study/Myinsta.kt
@@ -0,0 +1,11 @@
+package com.trace.trace_study
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Myinsta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_myinsta)
+    }
+}
